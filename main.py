076fead1735f8d2240393fa932af3623b33b0781
Skiles6389 <130098448+Skiles6389@users.noreply.github.com>
2023-12-06 12:55:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def find_arbitrage_opportunity(rate_matrix):
     rate_1 = rate_matrix[0][1]
-    rate_2 = rate_matrix[1][2]
+    rate_2 = rate_matrix[1][2]    #Calculates the potential profit
     rate_3 = rate_matrix[2][0]
 
     arbitrage_opportunity = rate_1 * rate_2 * rate_3
@@ -9,7 +9,7 @@ def find_arbitrage_opportunity(rate_matrix):
 
 def execute_arbitrage(balance, rate_matrix):
     amount_to_trade = balance
-
+                                             #Would be the execution of the trade if there was opportunity
     amount_bought_1 = amount_to_trade
     amount_sold_2 = amount_bought_1 / rate_matrix[0][1]
     amount_sold_3 = amount_sold_2 / rate_matrix[1][2]
@@ -20,9 +20,9 @@ def execute_arbitrage(balance, rate_matrix):
     print(f"Final balance: {amount_bought_back_1}")
 def main():
     rate_matrix = [
-        [1, 2, 0.5],
+        [1, 2, 0.5],        #would be the values of real-time currencies,  or the data acquired from CCXT
         [0.5, 1, 3],
-        [2, 0.33, 1]
+        [2, 0.33, 1]          #These numbers would be what calculates the arbitrage
     ]
     balance = 1
 
@@ -34,9 +34,13 @@ def main():
             execute_arbitrage(balance, rate_matrix)
 
 
-            time.sleep(10)
+            time.sleep(10)         #Delay time would be more frequent or less frequent depending how efficient the finsihed code (With CCXT) would be
 
 
 if __name__ == "__main__":
     import time
     main()
+
+#TROUBLESHOOTING NOTES:
+# ERROR: Could not build wheels for aiohttp, which is required to install pyproject.toml-based projects
+# -Final error received when trying to install CCXT
